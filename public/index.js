d44c33dc1a77125a3fb38c5d73ce98830c289791
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2023-05-30 04:16:37 -0700

    Nav Bar set

--- public/index.js
+++ public/index.js
@@ -2,15 +2,13 @@ const burger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 
 burger.addEventListener('click', () => {
-    if(menu.classList.contains('hidden')) {
+    if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
-    }
-    else {
+    } else {
         menu.classList.add('hidden');
         menu.classList.remove('bg-black');
     }
-})
-
+});
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -24,5 +22,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('nav');
+    const offsetHeight = document.querySelector('header').offsetHeight;
 
-
+    if (window.pageYOffset > offsetHeight) {
+        navbar.classList.remove('md:bg-transparent');
+        navbar.classList.add('md:bg-black', 'top-0', 'fixed');
+    } else {
+        navbar.classList.remove('md:bg-black', 'top-0', 'fixed');
+        navbar.classList.add('md:bg-transparent');
+    }
+});
