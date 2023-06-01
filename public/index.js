975704db6f4a58f9d4a6e9b96359a972793e2804
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2023-06-01 02:20:52 -0700

    Home Page demo

--- public/index.js
+++ public/index.js
@@ -1,14 +1,19 @@
 const burger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
+const navbar = document.querySelector('nav');
 
 burger.addEventListener('click', () => {
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
+        navbar.classList.add('bg-black');
     } else {
         menu.classList.add('hidden');
         menu.classList.remove('bg-black');
+        navbar.classList.remove('bg-black');
+
     }
 });
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -23,7 +28,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('nav');
+   
     const offsetHeight = document.querySelector('header').offsetHeight;
 
     if (window.pageYOffset > offsetHeight) {
