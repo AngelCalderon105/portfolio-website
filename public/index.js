90856628e7e5fa198b075cb77114bed0edf58e2f
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2023-06-01 17:02:14 -0700

    Added Contact

--- public/index.js
+++ public/index.js
@@ -6,11 +6,11 @@ burger.addEventListener('click', () => {
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
         navbar.classList.add('bg-black');
-        navbar.classList.remove('bg-transparent');
+       
     } else {
         menu.classList.add('hidden');
         navbar.classList.remove('bg-black');
-        navbar.classList.add('bg-transparent');
+       
        
 
     }
@@ -34,11 +34,11 @@ window.addEventListener('scroll', function() {
     const offsetHeight = document.querySelector('header').offsetHeight;
 
     if (window.pageYOffset > offsetHeight) {
-        navbar.classList.remove('bg-transparent');
+       
         navbar.classList.add('bg-black');
     } else {
         navbar.classList.remove('bg-black');
-        navbar.classList.add('bg-transparent');
+       
     }
 });
 
